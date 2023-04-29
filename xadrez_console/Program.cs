683946a2ebc687f710f8baa43c0a27a72e044cae
Marcos Alcântara <malcanoficial@gmail.com>
2023-04-28 22:57:28 -0300

    Criando classe Posicao

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao: " + p);
+
+            Console.ReadLine();
+        }
+    }
+}
